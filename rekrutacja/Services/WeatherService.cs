31439b4f9728465131a95cf21a6d81094364b696
Pompolinii <marcelinwijas@gmail.com>
2024-12-02 23:23:18 +0100

    gotow

--- rekrutacja/Services/WeatherService.cs
+++ rekrutacja/Services/WeatherService.cs
@@ -96,15 +96,5 @@ namespace rekrutacja.Services
 
   
 
-    public class WeatherData
-    {
-        public double Temp { get; set; }
-        public double TempMin { get; set; }
-        public double TempMax { get; set; }
-        public string Description { get; set; }
-        public int Humidity { get; set; }
-
-        public string Country { get; set; }
-        public string State { get; set; } 
-    }
+   
 }
